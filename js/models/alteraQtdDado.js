758d17698a8ed2fcd53adf8d43bb1312b1788ce7
Heitor da Costa Cândido <151862970+Hekth@users.noreply.github.com>
2024-02-03 16:18:51 -0300

    Delete js directory

--- js/models/alteraQtdDado.js
+++
@@ -1,9 +0,0 @@
-import { AlteraQtd } from "../controllers/alteraQtd-controller.js";
-export class AlteraQtdDados extends AlteraQtd {
-    alteraValor(decrementa = false, quantidade = 1) {
-        let soma = decrementa ? this.converteValor() - quantidade : this.converteValor() + quantidade;
-        if (soma > 0 && soma <= 100) {
-            this.qtdDado.innerHTML = `${soma}d`;
-        }
-    }
-}
